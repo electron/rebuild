0bb8bdc3cf18957205cc294b73ea4c994770c734
Paul Betts <paul@paulbetts.org>
2015-05-04 01:01:35 -0700

    Implement rebuilding native modules

--- src/main.js
+++ src/main.js
@@ -10,16 +10,39 @@ const fs = promisify(require('fs'));
 
 const getHeadersRootDirForVersion = (version) => {
   return path.resolve(__dirname, 'headers');
-}
+};
+
+const checkForInstalledHeaders = async function(nodeVersion, headersDir) {
+  const canary = path.join(headersDir, '.node-gyp', nodeVersion, 'common.gypi');
+  let stat = await fs.stat(canary);
+  
+  if (!stat) throw new Error("Canary file 'common.gypi' doesn't exist");
+  return true;
+};
+
+const spawnWithHeadersDir = async (cmd, args, headersDir) => {
+  let env = _.extend({}, process.env, { HOME: headersDir });
+  if (process.platform === 'win32')  {
+    env.USERPROFILE = env.HOME;
+  }
+  
+  try {
+    return await spawn({cmd, args, opts: {env}});
+  } catch (e) {
+    if (e.stdout) console.log(e.stdout);
+    if (e.stderr) console.log(e.stderr);
+    
+    throw e;
+  }
+};
 
 export async function installNodeHeaders(nodeVersion, nodeDistUrl=null, headersDir=null) {
   headersDir = headersDir || getHeadersRootDirForVersion(nodeVersion);
   let distUrl = nodeDistUrl || 'https://gh-contractor-zcbenz.s3.amazonaws.com/atom-shell/dist';
 
-  let canary = path.join(headersDir, '.node-gyp', nodeVersion, 'common.gypi');
   try {
-    let stat = await fs.stat(canary)
-    if (stat) return true;
+    await checkForInstalledHeaders(headersDir);
+    return;
   } catch (e) { }
   
   let cmd = 'node';
@@ -29,11 +52,20 @@ export async function installNodeHeaders(nodeVersion, nodeDistUrl=null, headersD
     `--arch=${process.arch}`,
     `--dist-url=${distUrl}`
   ];
-
-  let env = _.extend({}, process.env, { HOME: headersDir });
-  if (process.platform === 'win32')  {
-    env.USERPROFILE = env.HOME;
-  }
   
-  await spawn({cmd, args, opts: {env}});
+  await spawnWithHeadersDir(cmd, args, headersDir);
+}
+
+export async function rebuildNativeModules(nodeVersion, nodeModulesPath=null, headersDir=null) {
+  headersDir = headersDir || getHeadersRootDirForVersion(nodeVersion);
+  await checkForInstalledHeaders(nodeVersion, headersDir);
+  
+  let cmd = 'node';
+  let args = [
+    require.resolve('npm/bin/npm-cli'), 'rebuild', 
+    `--target=${nodeVersion}`, 
+    `--arch=${process.arch}`
+  ];
+  
+  await spawnWithHeadersDir(cmd, args, headersDir);
 }
--- test/main.js
+++ test/main.js
@@ -3,8 +3,9 @@ import path from 'path';
 import promisify from '../lib/promisify';
 const fs = promisify(require('fs'));
 const rimraf = promisify(require('rimraf'));
+const cp = promisify(require('ncp').ncp);
 
-import {installNodeHeaders} from '../lib/main.js';
+import {installNodeHeaders, rebuildNativeModules} from '../lib/main.js';
 
 describe('installNodeHeaders', function() {
   this.timeout(30*1000);
@@ -21,5 +22,42 @@ describe('installNodeHeaders', function() {
     await installNodeHeaders('0.25.2', null, targetHeaderDir);
     let canary = await fs.stat(path.join(targetHeaderDir, '.node-gyp', '0.25.2', 'common.gypi'));
     expect(canary).to.be.ok
+  });
+});
+
+describe('rebuildNativeModules', function() {
+  this.timeout(60*1000);
+  
+  it('Rebuilds native modules against 0.25.2', async () => {
+    const targetHeaderDir = path.join(__dirname, 'testheaders');
+    const targetModulesDir = path.join(__dirname, 'test_modules');
+    
+    try {
+      if (await fs.stat(targetHeaderDir)) {
+        await rimraf(targetHeaderDir);
+      }  
+    } catch (e) { }
+    
+    await fs.mkdir(targetHeaderDir);
+    
+    try {
+      if (await fs.stat(targetModulesDir)) {
+        await rimraf(targetModulesDir);
+      }
+    } catch (e) { }
+    
+    console.log("Got here!");
+    
+    await installNodeHeaders('0.25.2', null, targetHeaderDir);
+    let canary = await fs.stat(path.join(targetHeaderDir, '.node-gyp', '0.25.2', 'common.gypi'));
+    expect(canary).to.be.ok
+    
+    // Copy our own node_modules folder to a fixture so we don't trash it
+    await cp(path.resolve(__dirname, '..', 'node_modules'), targetModulesDir);
+    
+    canary = await fs.stat(path.join(targetModulesDir, 'babel'));
+    expect(canary).to.be.ok;
+    
+    await rebuildNativeModules('0.25.2', targetModulesDir, targetHeaderDir);
   });
 });
