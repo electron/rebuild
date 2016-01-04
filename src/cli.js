87350c81f6703406349e652ae3b49a2c82bcb1b9
Félicien FRANCOIS <felicien@tweakstyle.com>
2016-01-04 20:02:37 +0100

    Handle relative paths for electron-prebuilt-dir parameter

--- src/cli.js
+++ src/cli.js
@@ -27,6 +27,8 @@ const argv = require('yargs')
 
 if (!argv.e) {
   argv.e = path.join(__dirname, '..', '..', 'electron-prebuilt');
+} else {
+  argv.e = path.resolve(process.cwd(), argv.e);
 }
 
 if (!argv.v) {
