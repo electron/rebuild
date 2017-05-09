9dc5d56c46b5989f3a732144f644705746055eb1
Samuel Attard <samuel.r.attard@gmail.com>
2017-05-09 16:50:58 +1000

    Fix default export for backwards compat

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-export { rebuild, rebuildNativeModules } from './rebuild';
+import { rebuild, rebuildNativeModules } from './rebuild';
 
 export const installNodeHeaders = () => Promise.resolve();
 export const shouldRebuildNativeModules  = () => Promise.resolve(true);
 export const preGypFixRun = () => Promise.resolve();
+export { rebuild, rebuildNativeModules };
+export default rebuild;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
