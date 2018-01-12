91741d3eb5b1ab3e23f4ee9982f5fe25b7a5f8ba
Samuel Attard <sattard@atlassian.com>
2018-01-12 15:49:27 +1100

    Node 8 process override

--- src/prebuild-shim.ts
+++ src/prebuild-shim.ts
@@ -2,7 +2,7 @@ process.argv.splice(1, 1);
 
 // This tricks prebuild-install into not validating on the
 // 1.8.x and 8.x ABI collision
-process.versions.modules = '-1';
+Object.defineProperty(process.versions, 'modules', { value: '-1', writable: false });
 
 /* tslint:disable */
 require(process.argv[1]);
