8698fd70834d56114963f92ecd354a1504cd1dc9
Mark Lee <electronjs@lazymalevolence.com>
2021-08-22 11:51:31 -0700

    refactor: extract generic arch functions to its own file

--- src/module-type/prebuildify.ts
+++ src/module-type/prebuildify.ts
@@ -1,8 +1,9 @@
-import { execSync } from 'child_process';
 import debug from 'debug';
 import fs from 'fs-extra';
 import path from 'path';
 
+import { getNodeArch } from '../arch';
+import { NativeModule } from '.';
 import { NodeAPI } from '../node-api';
 
 export type DevDependencies = Record<string, string>;
@@ -14,48 +15,15 @@ export function isPrebuildifyNativeModule(devDependencies: DevDependencies): boo
   return devDependencies.hasOwnProperty('prebuildify');
 }
 
-/**
- * Runs the `uname` command and returns the trimmed output.
- *
- * Copied from `@electron/get`.
- */
-export function uname(): string {
-  return execSync('uname -m')
-    .toString()
-    .trim();
-}
-
-export type ConfigVariables = {
-  arm_version?: string;
-}
-
-/**
- * Generates an architecture name that would be used in an Electron or Node.js
- * download file name.
- *
- * Copied from `@electron/get`.
- */
-export function getNodeArch(arch: string, configVariables: ConfigVariables): string {
-  if (arch === 'arm') {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    switch (configVariables.arm_version) {
-      case '6':
-        return uname();
-      case '7':
-      default:
-        return 'armv7l';
-    }
-  }
-
-  return arch;
-}
-
 export function determineNativePrebuildArch(arch: string): string {
   if (arch === 'armv7l') {
     return 'arm';
   }
 
   return arch;
+}
+
+export class Prebuildify extends NativeModule {
 }
 
 /**
